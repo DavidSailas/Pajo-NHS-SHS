3cbf6571d0dabc4c21000cc12d2462f36eae4e3e
DavidSailas <161728603+DavidSailas@users.noreply.github.com>
2026-09-06 23:48:08 +0800

    Remove composer install from deployment script
    Removed composer install command from deployment script.

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing composer dependencies..."
-composer install --no-dev --optimize-autoloader --working-dir=/var/www/html
-
 echo "Caching config..."
 php artisan config:cache
 
